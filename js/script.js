c56999363d8bea15b4f830d97116f515ef9bdcae
MattiaPortanti <mattia.portantipm@gmail.com>
2022-02-21 17:53:59 +0100

    aggiunta commneto finale nel file js

--- js/script.js
+++ js/script.js
@@ -8,4 +8,5 @@ console.log(lastName);
 // Dichiaro e Assegno una variabile che può essere riassegnata (let) 
 let favouriteColor = prompt("Qual'è il tuo colore preferito");
 console.log(favouriteColor);
+// Inserisco all'interno del HTML, dentro l'elemento con id my-password l'output delle variabili + il 21 finale 
 document.getElementById("my-password").innerHTML = firstName + lastName + favouriteColor + ("21")  
