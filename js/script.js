521e3aa1c4bef14a4532251476e0d168b6b9442f
MattiaPortanti <mattia.portantipm@gmail.com>
2022-02-21 17:41:40 +0100

    completati i primi requisiti del esercizio, nel prompt ci vengono richiuesti nome cognome e colore preferito e infine viene stamapata la password sul file html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-document.writeln("Hello World!");
+const firstName = prompt("Inserisci il tuo nome");
+console.log(firstName);
+const lastName = prompt("Inserisci il tuo cognome");
+console.log(lastName);
+
+let favouriteColor = prompt("Qual'è il tuo colore preferito");
+console.log(favouriteColor);
+document.getElementById("my-password").innerHTML = firstName + lastName + favouriteColor;  
