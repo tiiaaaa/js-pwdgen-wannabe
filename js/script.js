95b6e2d95c18b7ba901f997f6a8446951e69f7ae
MattiaPortanti <mattia.portantipm@gmail.com>
2022-02-21 17:50:44 +0100

    aggiunto il 21 alla fine della password stamapata sul file html e aggiunti commenti per descrivere le varie funzioni

--- js/script.js
+++ js/script.js
@@ -1,8 +1,11 @@
+// Dichiaro e Assegno una variabile che non può essere modificata (const)
 const firstName = prompt("Inserisci il tuo nome");
+// Richiamo la variabile tramite il nome che le ho assegnato
 console.log(firstName);
+
 const lastName = prompt("Inserisci il tuo cognome");
 console.log(lastName);
-
+// Dichiaro e Assegno una variabile che può essere riassegnata (let) 
 let favouriteColor = prompt("Qual'è il tuo colore preferito");
 console.log(favouriteColor);
-document.getElementById("my-password").innerHTML = firstName + lastName + favouriteColor;  
+document.getElementById("my-password").innerHTML = firstName + lastName + favouriteColor + ("21")  
